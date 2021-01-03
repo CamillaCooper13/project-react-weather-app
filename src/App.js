c046bdf155500e82bde6e506508eba38ca3f759d
CamillaCooper13 <71074056+CamillaCooper13@users.noreply.github.com>
2021-01-03 15:20:48 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     Weather App
+    <footer>
+      This project was coded by{" "}
+      <a href="https://www.linkedin.com/in/camilla-cooper-7a437619b/" target="_blanc">
+  Camilla Cooper 
+  </a>{" "}
+      and is {" "}
+    <a href="https://github.com/CamillaCooper13/project-react-weather-app"
+    target="_blanc"
+    >
+open-sourced on GitHub
+    </a>
+    </footer>
     </div>
   );
 }
 
-export default App;
+
