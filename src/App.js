50034280f3e4655c74c7fa17d98d50c198693620
CamillaCooper13 <71074056+CamillaCooper13@users.noreply.github.com>
2021-01-07 12:26:33 +0000

    Added icon component, changed styling

--- src/App.js
+++ src/App.js
@@ -11,13 +11,11 @@ export default function App() {
     <footer>
       This project was coded by{" "}
       <a href="https://www.linkedin.com/in/camilla-cooper-7a437619b/" target="_blanc">
-  Camilla Cooper 
+   Camilla Cooper 
   </a>{" "}
       and is {" "}
     <a href="https://github.com/CamillaCooper13/project-react-weather-app"
-    target="_blanc"
-    >
-open-sourced on GitHub
+    target="_blanc"> open-sourced on GitHub
     </a>
     </footer>
     </div>
